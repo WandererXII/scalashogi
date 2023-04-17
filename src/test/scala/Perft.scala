3baf4a934d442be0c74fd350ea1a0797317e5759
WandererXII <43043181+WandererXII@users.noreply.github.com>
2023-04-18 01:44:50 +0200

    Allow loging perft

--- src/test/scala/Perft.scala
+++ src/test/scala/Perft.scala
@@ -14,11 +14,13 @@ object Perft {
       .moveActorsOf(sit.color)
       .flatMap(_.toUsis)
 
-  def perft(game: Game, depth: Int): Int =
+  def perft(game: Game, depth: Int, log: Boolean = false): Int =
     if (depth > 0) {
       val mds: List[Usi] = moves(game.situation) ::: drops(game.situation)
       mds.foldLeft(0) { (p, u) =>
-        p + perft(game(u).toOption.get, depth - 1)
+        val cnt = perft(game(u).toOption.get, depth - 1)
+        if (log) println(s"${u.usi.padTo(5, ' ')} - $cnt")
+        p + cnt
       }
     } else 1
 
