d8ba813c7306c48abcd725b9e277296f78e3e4c3
WandererXII <43043181+WandererXII@users.noreply.github.com>
2024-02-20 01:34:12 +0100

    scalafmt

--- src/test/scala/StatusTest.scala
+++ src/test/scala/StatusTest.scala
@@ -1,9 +1,9 @@
 package shogi
 
 class StatusTest extends ShogiTest {
-    "Status id" should {
-        "be unique" in {
-            Status.all.length must_== Status.all.map(_.id).toSet.size
-        }
+  "Status id" should {
+    "be unique" in {
+      Status.all.length must_== Status.all.map(_.id).toSet.size
     }
+  }
 }
